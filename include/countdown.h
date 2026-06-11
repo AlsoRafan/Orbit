dd52f46b451bf6646a6dec5bfa343d409f5c080d
Rafay Ahmed <25-se-017@student.hitecuni.edu.pk>
2026-06-11 19:06:29 +0000

    stopwatch and the countdown func added

--- include/countdown.h
+++ include/countdown.h
@@ -1,2 +1,25 @@
 #pragma once
-void Countdown();
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <iomanip>
+
+class Timer {
+protected:
+    int time_sec;
+public:
+    Timer(int t = 0) : time_sec(t) {}
+    void        set_time();
+    virtual void start() = 0;
+    virtual ~Timer() = default;
+};
+
+class CountDown : public Timer {
+public:
+    CountDown(int t = 0) : Timer(t) {}
+    void display() const;
+    void start() override;
+};
+
+int countdown();
--- src/countdown.cpp
+++ src/countdown.cpp
@@ -1,7 +1,48 @@
-#include <iostream>
 #include "../include/countdown.h"
-using namespace std;
 
-void Countdown(){
-    cout<<endl<<"hello from the count down";
+void Timer::set_time() {
+    int choice, t;
+    std::cout << "\nChoose time unit:\n"
+              << "  1. Seconds\n"
+              << "  2. Minutes\n"
+              << "  3. Hours\n"
+              << "Enter choice: ";
+    std::cin >> choice;
+    std::cout << "Enter time: ";
+    std::cin >> t;
+
+    switch (choice) {
+        case 1: time_sec = t;        break;
+        case 2: time_sec = t * 60;   break;
+        case 3: time_sec = t * 3600; break;
+        default:
+            std::cout << "Invalid choice.\n";
+            time_sec = 0;
+    }
+}
+
+void CountDown::display() const {
+    int h = time_sec / 3600;
+    int m = time_sec % 3600 / 60;
+    int s = time_sec % 60;
+    std::cout << std::setfill('0')
+              << std::setw(2) << h << ":"
+              << std::setw(2) << m << ":"
+              << std::setw(2) << s << "\r" << std::flush;
+}
+
+void CountDown::start() {
+    while (time_sec > 0) {
+        display();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        time_sec--;
+    }
+    std::cout << "\nTime's up!\n";
+}
+
+int countdown() {
+    CountDown c;
+    c.set_time();
+    c.start();
+    return 0;
 }
