fd90f4805d058ce8c758362f0d93114b3d506ca3
Rafay Ahmed <25-se-017@student.hitecuni.edu.pk>
2026-06-21 10:59:00 -0700

    Add Alarm option to main menu

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "include/stopwatch.h"
 #include "include/countdown.h"
-
+#include "include/alarm.h"
 using namespace std;
 
 int main() {
@@ -11,6 +11,7 @@ int main() {
  
               << "  1. Stopwatch\n"
               << "  2. Countdown\n"
+              << "  3. Alarm\n"
               << "  Enter choice: ";
 
     cin >> choice;
@@ -18,7 +19,8 @@ int main() {
     switch (choice) {
         case 1: stopwatch(); break;
         case 2: countdown(); break;
-        default: std::cout << "  Invalid choice.\n"; break;
+        case 3: Alarm(); break;
+        default: cout << "  Invalid choice.\n"; break;
     }
 
     return 0;
